dbbfda8f78fb6d90e0608dd9319367ec099ef0cb
Kivilev Denis <d.kivilev@qiwi.com>
2020-03-26 15:55:50 +0300

    add unit tests

---
+++ Иное. Лекция 2. Тестирование в СУБД/run_unit_tests.sql
@@ -0,0 +1,21 @@
+-- ********** СПОСОБЫ ЗАПУСКА UNIT-тестов *****************
+
+-- Запуск всех тестов во всех пакетах (полный регресс)
+select *
+  from table(ut.run());
+
+
+-- Запуск тестов в конкретном пакете
+select *
+  from table(ut.run('test_wallet_pack'));
+
+
+-- Запуск тестов в одном с выбором формата отчета
+select *
+  from table(ut.run(a_reporter => ut_teamcity_reporter()));
+
+
+-- Формирование html-отчета на покрытие кода тестами
+select *
+  from ut.run(a_reporter => ut_coverage_html_reporter());
+
